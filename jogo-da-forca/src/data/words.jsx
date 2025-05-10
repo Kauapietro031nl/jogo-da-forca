58d2179a9b2337b930afc84843b3adbbc782d5aa
Kauã Pietro <kauapietro2608@gmail.com>
2025-05-10 17:07:02 -0300

    Primeiro commit

---
+++ jogo-da-forca/src/data/words.jsx
@@ -0,0 +1,34 @@
+export const wordsList = {
+  carro: [
+    "Volante", "Freio", "Farol", "Marcha", "Motor", 
+    "Pneu", "Retrovisor", "Porta", "Buzina", "Cinto"
+  ],
+  fruta: [
+    "Maçã", "Banana", "Uva", "Morango", "Abacaxi", 
+    "Melancia", "Laranja", "Pera", "Kiwi", "Manga"
+  ],
+  corpo: [
+    "Cabeça", "Braço", "Perna", "Coração", "Olho", 
+    "Nariz", "Boca", "Mão", "Pé", "Barriga"
+  ],
+  alimento: [
+    "Arroz", "Feijão", "Pão", "Queijo", "Carne", 
+    "Frango", "Batata", "Tomate", "Leite", "Ovo"
+  ],
+  animais: [
+    "Cachorro", "Gato", "Leão", "Elefante", "Cavalo", 
+    "Macaco", "Pássaro", "Peixe", "Urso", "Tigre"
+  ],
+  roupas: [
+    "Camiseta", "Calça", "Vestido", "Tênis", "Sapato", 
+    "Meia", "Casaco", "Chapéu", "Blusa", "Saia"
+  ],
+  esportes: [
+    "Futebol", "Basquete", "Vôlei", "Tênis", "Natação", 
+    "Ciclismo", "Corrida", "Skate", "Boxe", "Judô"
+  ],
+  casa: [
+    "Cama", "Mesa", "Cadeira", "Sofá", "Fogão", 
+    "Geladeira", "Televisão", "Chuveiro", "Janela", "Porta"
+  ]
+};
